e63ef959fadbf83da02e749457766890c58023fb
PRV3699 <pranavvidap31@gmail.com>
2022-05-13 12:16:27 +0530

    second commit

--- src/pages/booklist.js
+++ src/pages/booklist.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
-import { getBooks } from "../services/book"
-
+import { getBooks } from "../services/book";
+import { deleteBook } from "../services/book";
+import{ createBook } from "../services/book";
+import { useNavigate } from "react-router-dom"
+import {DropdownButton,Dropdown} from 'react-bootstrap'
 const BookListPage =(props)=>{
     const [books, setBooks] = useState([])
+    const navigate=useNavigate()
     useEffect(() => {
         loadBooks()
     }, [])
@@ -13,35 +17,66 @@ const BookListPage =(props)=>{
            setBooks(result)
         }
     }
-    
+    const onDeleteBook = async (id) => {
+        const result = await deleteBook(id)
+        if(result)
+        {
+            window.location.reload();
+            alert('Book is deleted');
+        }
+        else {
+            alert('You are not an admin');
+        }
+    }
+    // const UpdatebookPage = async (id) => {
+        
+
+    // }
+   
+    const logout=()=>{
+        sessionStorage.removeItem('token')
+        sessionStorage.removeItem('id')
+        
+        navigate('/signin')
+    }
     return <div className="container">
          <h2 className='header'>List of Books</h2>
+         <DropdownButton id="dropdown-basic-button" title="Dashboard" style={{float:'right'}}>
+        {/* <Dropdown.Item href="/profilepage">Profile</Dropdown.Item> */}
+        <Dropdown.Item  href="/create-book">Entry Book</Dropdown.Item>
+        <Dropdown.Item onClick={logout} href="#/action-2">Logout</Dropdown.Item>
+        </DropdownButton>
          <div className='book-table'>
-             <table style={{border:'2px solid'}}>
+         <table className="table table-success table-striped">
+             <table className="itable" style={{border:'2px solid'}}>
                  <thead>
                      <tr style={{border:'1px solid'}}>
                          <th>Title</th>
-                         <th>author</th>
-                         <th>description</th>
-                         <th>quantity</th>
+                         <th>Author</th>
+                         <th>Description</th>
+                         <th>Quantity</th>
+                         <th>   Operations</th>
                          </tr>
                          </thead>
                          <tbody>
                              {books.map((book) => {
-                                 const {bookId, title, author,description,quantity} = book
+                                 const {id, title, author,description,quantity} = book
                                  return (
-                                 <tr>
+                                     <tr>
                                      
                                      <td>{title}</td>
                                      <td>{author}</td>
                                      <td>{description}</td>
                                      <td>{quantity}</td>
+                                     <td><button onClick={()=>onDeleteBook(id)} className="btn btn-success">delete</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                     <button className="btn btn-success">update</button></td>
                                      </tr>
                                      )
                                      })}
-                                     </tbody>
-                                     </table>
-                                     </div>
-                                     </div>
-                          }
+                            </tbody>
+            </table>
+            </table>
+        </div>
+        </div>
+}
 export default BookListPage
--- src/pages/createbook.js
+++ src/pages/createbook.js
@@ -1,4 +1,95 @@
-const BookCreatePage =(props)=>{
-    return <div></div> 
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { createBook } from '../services/book'
+
+const CreateBookPage = (props) => {
+  // state
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity]= useState(0)
+
+  const navigate = useNavigate()
+
+  const onCreateBook = async () => {
+    if (title.length === 0) {
+      alert('set title')
+    } else if (author.length === 0) {
+      alert('set category')
+    } else if (description.length === 0) {
+        alert('set category')
+    } else if (quantity.length === 0) {
+        alert('set quantity')
+    } else {
+      const result = await createBook(title, author, description, quantity)
+      if (result) {
+        navigate('/dashboard')
+      } else {
+        alert('You are not admin')
+      }
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="header">Create Book</h1>
+      <div className="form">
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input
+            onChange={(e) => {
+              setTitle(e.target.value)
+            }}
+            type="text"
+            className="form-control"
+          />{' '}
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Author</label>
+          <input
+            onChange={(e) => {
+              setAuthor(e.target.value)
+            }}
+            type="text"
+            className="form-control"
+          />{' '}
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <input
+            onChange={(e) => {
+              setDescription(e.target.value)
+            }}
+            type="text"
+            className="form-control"
+          />{' '}
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Quantity</label>
+          <input
+            onChange={(e) => {
+              setQuantity(e.target.value)
+            }}
+            type="number"
+            className="form-control"
+          />
+        </div>
+        
+
+        <div className="mb-3">
+          <button onClick={onCreateBook} className="btn btn-success">Save</button>
+          <Link
+            to="/dashboard"
+            style={{ marginLeft: '15px' }}
+            className="btn btn-danger">Cancel
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
-export default BookCreatePage
+
+export default CreateBookPage
